a045bc3e8127a4ca825716685018ff389d426e29
Satit Rianpit <rianpit@gmail.com>
2017-01-18 10:35:46 +0700

    add add customer

--- models/customer.js
+++ models/customer.js
@@ -15,5 +15,52 @@ module.exports = {
       });
       
     });
+  },
+  getCustomerTypes(db) {
+    return new Promise((resolve, reject) => {
+      db.getConnection((err, conn) => {
+        if (err) {
+          reject(err);
+        } else {
+          let sql = `SELECT * FROM customer_types`;
+          conn.query(sql, [], (err, rows) => {
+            if (err) reject(err);
+            else resolve(rows);
+          });
+          conn.release();
+        }
+      });
+      
+    });
+  },
+
+  save(db, customer) {
+    return new Promise((resolve, reject) => {
+      db.getConnection((err, conn) => {
+        if (err) {
+          reject(err);
+        } else {
+          let sql = `
+          INSERT INTO customers(first_name, last_name,
+          sex, customer_type_id, email, telephone, image) 
+          VALUES(?, ?, ?, ?, ?, ?, ?)
+          `;
+          conn.query(sql, [
+            customer.first_name,
+            customer.last_name,
+            customer.sex,
+            customer.customer_type_id,
+            customer.email,
+            customer.telephone,
+            customer.image
+          ], (err, rows) => {
+            if (err) reject(err);
+            else resolve(rows);
+          });
+          conn.release();
+        }
+      });
+      
+    });
   }
 }
--- routes/customers.js
+++ routes/customers.js
@@ -2,6 +2,40 @@ var express = require('express');
 var router = express.Router();
 var Customer = require('../models/customer');
 
+router.post('/', (req, res, next) => {
+  let first_name = req.body.first_name;
+  let last_name = req.body.last_name;
+  let sex = req.body.sex;
+  let customer_type_id = req.body.customer_type_id;
+  let telephone = req.body.telephone;
+  let email = req.body.email;
+  let image = req.body.image;
+
+  if (first_name && last_name && customer_type_id) {
+    let customer = {
+      first_name: first_name,
+      last_name: last_name,
+      sex: sex,
+      customer_type_id: customer_type_id,
+      telephone: telephone,
+      email: email,
+      image: image
+    }
+
+    let db = req.db;
+    Customer.save(db, customer)
+      .then(() => {
+        res.send({ ok: true });
+      }, (error) => { 
+        res.send({ ok: false, error: error });
+       });
+
+  } else {
+    res.send({ ok: false, error: 'ข้อมูลไม่ครบถ้วน' });
+  }
+
+});
+
 router.get('/', function(req, res, next) {
   let db = req.db;
   Customer.all(db)
@@ -18,6 +52,16 @@ router.get('/', function(req, res, next) {
         customers.push(obj);
       });
       res.send({ ok: true, rows: customers });
+    }, (error) => {
+      res.send({ ok: false, error: error });
+    });
+});
+
+router.get('/customer-types', function(req, res, next) {
+  let db = req.db;
+  Customer.getCustomerTypes(db)
+    .then((rows) => {
+      res.send({ ok: true, rows: rows });
     }, (error) => {
       res.send({ ok: false, error: error });
     });
